01d397bd370dd0d3287ba609ae1479aa7fad4606
LixiangLee <lxiaolyoux@qq.com>
2018-05-28 23:42:29 +0800

    CodeTemplate

---
+++ src/CodeTemplateTest.java
@@ -0,0 +1,20 @@
+
+
+/** 
+ * @ClassName: CodeTemplateTest 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author A18ccms a18ccms_gmail_com 
+ * @date 2018年5月28日 下午11:39:12 
+ *  
+ */
+public class CodeTemplateTest {
+
+	/** 
+	 * <p>Title: </p> 
+	 * <p>Description: </p>  
+	 */
+	public CodeTemplateTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/lx/practice/git/sub/CodeTemplateTest.java
@@ -0,0 +1,39 @@
+package com.lx.practice.git.sub;
+
+/**
+ * <p> Title:   </p>
+ * <p> Description: </p>
+ * @author Lixiang Lee
+ * @create time 2018年5月28日 下午11:41:36
+ * @version 1.00
+ * @fix record 
+ * <pre>
+ * 版本   修改人    修改时间    修改内容描述
+ * ----------------------------------------
+ * 1.00 "LixiangLee" 下午11:41:36  <p> 一句话描述   </p>
+ * ----------------------------------------
+ * </pre>
+ */
+public class CodeTemplateTest {
+
+	/** 
+	 * <p>Title: </p> 
+	 * <p>Description: </p>  
+	 */
+	public CodeTemplateTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @param @param args    设定文件 
+	 * @return void    返回类型 
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
